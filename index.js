2fd552ff497b383c6e3f3c31af60040dfca11e86
zxczxc <Tealwater@yandex.ru>
2025-05-19 18:54:51 +0300

    Добавлен middleware для обработки CORS-запросов, позволяющий разрешать доступ с любых источников и поддерживающий необходимые HTTP-методы.

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ const rl = readline.createInterface({
 app.use(bodyParser.json());
 
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
 app.use('/api', apiRoutes);
 
 
